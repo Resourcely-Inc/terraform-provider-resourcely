e5b77afeaa031bd758dacf8cc145c6e9a3395abf
Aladdin Almubayed <aladdin.almubayed@resourcely.io>
2023-09-21 15:26:51 -0700

    add docs

--- examples/resources/scaffolding_example/resource.tf
+++ examples/resources/scaffolding_example/resource.tf
@@ -1,3 +1,4 @@
-resource "scaffolding_example" "example" {
-  configurable_attribute = "some-value"
+# for more information on Authoring Your Own Blueprints https://docs.resourcely.com/getting-started/using-resourcely/setting-up-blueprints/authoring-your-own-blueprints
+resource "resourcely_blueprint_template" "example" {
+  onfigurable_attribute = "some-value"
 }
