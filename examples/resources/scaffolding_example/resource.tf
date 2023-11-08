3c21ffdd8c0c54c7e3bbd76a642d1dcef71685fc
Nathan Glass <nathan.glass@resourcely.io>
2023-11-08 14:54:39 -0800

    Docs/examples changes

--- examples/resources/scaffolding_example/resource.tf
+++ examples/resources/scaffolding_example/resource.tf
@@ -1,4 +1,4 @@
 # for more information on Authoring Your Own Blueprints https://docs.resourcely.com/getting-started/using-resourcely/setting-up-blueprints/authoring-your-own-blueprints
-resource "resourcely_blueprint_template" "example" {
+resource "resourcely_blueprint" "example" {
   configurable_attribute = "some-value"
 }
