273f12ff3f82515e18671d1d10ef38aace5b0e77
David R. Bild <david.bild@resourcely.io>
2024-08-08 17:17:56 -0500

    provider: copy JSON validators/planmodifiers from the internal provider

---
+++ internal/provider/planmodifiers.go
@@ -0,0 +1,60 @@
+package provider
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+)
+
+func SuppressEquivalentJsonDiffs() planmodifier.String {
+	return suppressEquivalentJsonDiffs{}
+}
+
+type suppressEquivalentJsonDiffs struct{}
+
+func (m suppressEquivalentJsonDiffs) PlanModifyString(_ context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+	// if req.StateValue is equivalent to req.PlanValue, use the state value for resp.PlanValue
+	// else, use plan value for resp.PlanValue
+	resp.PlanValue = req.PlanValue
+
+	if IsKnown(req.PlanValue) && IsKnown(req.StateValue) {
+		isSame, err := isSameJson(req.PlanValue.ValueString(), req.StateValue.ValueString())
+		if err != nil {
+			resp.Diagnostics.AddError("Error comparing JSON values", err.Error())
+			return
+		}
+
+		if isSame {
+			resp.PlanValue = req.StateValue
+		}
+	}
+
+	return
+}
+
+func (m suppressEquivalentJsonDiffs) Description(_ context.Context) string {
+	return "The value of this attribute only changes if the json string fails a deep equal check."
+}
+
+// MarkdownDescription returns a markdown description of the plan modifier.
+func (m suppressEquivalentJsonDiffs) MarkdownDescription(_ context.Context) string {
+	return "The value of this attribute only changes if the json string fails a deep equal check."
+}
+
+func isSameJson(a string, b string) (bool, error) {
+	var aobj interface{}
+	var bobj interface{}
+
+	if err := json.Unmarshal([]byte(a), &aobj); err != nil {
+		return false, fmt.Errorf("error unmarshalling string: %s => %v", a, err.Error())
+	}
+
+	if err := json.Unmarshal([]byte(b), &bobj); err != nil {
+		return false, fmt.Errorf("error unmarshalling string: %s => %v", b, err.Error())
+	}
+
+	return reflect.DeepEqual(aobj, bobj), nil
+}
---
+++ internal/provider/validators.go
@@ -0,0 +1,50 @@
+package provider
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
+)
+
+var _ validator.String = stringIsJson{}
+
+// stringIsJson validates that a string is a valid JSON
+type stringIsJson struct{}
+
+func StringIsJSON() validator.String {
+	return stringIsJson{}
+}
+
+// Description describes the validation in plain text formatting.
+func (validator stringIsJson) Description(_ context.Context) string {
+	return "string must be valid JSON"
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (validator stringIsJson) MarkdownDescription(ctx context.Context) string {
+	return "string must be valid JSON"
+}
+
+// Validate performs the validation.
+func (v stringIsJson) ValidateString(ctx context.Context, request validator.StringRequest, response *validator.StringResponse) {
+	if request.ConfigValue.IsNull() || request.ConfigValue.IsUnknown() {
+		return
+	}
+
+	value := request.ConfigValue.ValueString()
+	var js json.RawMessage
+	isJSON := json.Unmarshal([]byte(value), &js) == nil
+
+	if !isJSON {
+		response.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
+			request.Path,
+			v.Description(ctx),
+			value,
+		))
+
+		return
+	}
+}
