e3f058f9156533cb980032726ee4d86ee69f289f
David R. Bild <david.bild@resourcely.io>
2024-08-08 17:18:59 -0500

    provider: add global values resource and test

--- internal/provider/util.go
+++ internal/provider/util.go
@@ -1,8 +1,12 @@
 package provider
 
 import (
+	"regexp"
+
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
+
+var UUID_REGEX = regexp.MustCompile("^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$")
 
 // IsKnown returns true if the String represents known, non-null
 // value.
